4c8fbd611ddcc95d3d3cf45db4cff4400d48557f
Laura Ferreira <99745361+qanon2409@users.noreply.github.com>
2022-06-17 10:34:06 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesRecursivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           int c=Convert.ToInt32(Console.ReadLine());
+            //FuncionInt(c, 1);
+            /*void FuncionInt(int a,int b)
+            {
+
+                 if(b!=10)
+                 {
+                     Console.WriteLine(a + b);
+                     FuncionInt(a, b + 1);
+                 }
+
+            }*/
+           SumarInt(c);
+           void SumarInt(int a)
+           {
+                
+                if(a==1)
+                {
+                    return;
+                }
+                else 
+                {
+                    c = (c-1) + a;
+                    Console.WriteLine(c);
+                    SumarInt(a-1);
+
+                }
+
+            }
+           Console.ReadKey();
+
+        }
+    }
+}
